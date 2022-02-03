2a5cb808ed404008b208db7e1c14142c88517001
lukemcneil <lukemcneil61@gmail.com>
2022-02-03 18:32:49 -0500

    run on loop

--- colorThresholds.m
+++ colorThresholds.m
@@ -9,61 +9,65 @@ close all;
 % original_image = cam.snapshot();
 % original_image = imresize(original_image, 0.5);
 % original_image = imgaussfilt(original_image, 3);
-original_image = imread("cube.png");
-
-imshow(original_image);
-
-edges = edge(rgb2gray(original_image), "canny");
-
-[L,Centers] = imsegkmeans(original_image,13);
-image = label2rgb(L, im2double(Centers));
-
-image = rgb2hsv(image); 
-imtool(image);
-h = image(:,:,1);
-s = image(:,:,2);
-v = image(:,:,3);
-
-red_mask = h > 0.92 & h < 1 & s > 0.5 & v > 0.2;
-orange_mask = h > 0.04 & h < 0.12 & s > 0.7;
-yellow_mask = h > 0.12 & h < 0.18;
-white_mask = v > 0.7 & s < 0.4;
-green_mask = h > 0.35 & h < 0.48;
-blue_mask = h > 0.55 & h < 0.65 & s > 0.5;
-black_mask = v < 0.4;
-
-se = strel("square", 25);
-red_mask = imopen(red_mask, se);
-orange_mask = imopen(orange_mask, se);
-yellow_mask = imopen(yellow_mask, se);
-white_mask = imopen(white_mask, se);
-green_mask = imopen(green_mask, se);
-blue_mask = imopen(blue_mask, se);
-
-subplot(3, 4, 1); imshow(original_image); title("original image");
-subplot(3, 4, 2); imshow(hsv2rgb(image)); title("k-means");
-subplot(3, 4, 3); imshow(image); title("hsv image");
-subplot(3, 4, 4); imshow(red_mask); title("red mask");
-subplot(3, 4, 5); imshow(orange_mask); title("orange mask");
-subplot(3, 4, 6); imshow(yellow_mask); title("yellow mask");
-subplot(3, 4, 7); imshow(white_mask); title("white mask");
-subplot(3, 4, 8); imshow(green_mask); title("green mask");
-subplot(3, 4, 9); imshow(blue_mask); title("blue mask");
-subplot(3, 4, 10); imshow(black_mask); title("black mask");
-subplot(3, 4, 11); imshow(edges); title("edges");
-
-masks(:,:,1) = red_mask;
-masks(:,:,2) = orange_mask;
-masks(:,:,3) = yellow_mask;
-masks(:,:,4) = white_mask;
-masks(:,:,5) = green_mask;
-masks(:,:,6) = blue_mask;
-
-grid = masksToGrid(masks, ["R", "O", "Y", "W", "G", "B"]);
-grid
+cam = webcam;
+while true
+    original_image = cam.snapshot;
+    original_image = imresize(original_image, 0.3);
+    
+    imshow(original_image);
+    
+    % edges = edge(rgb2gray(original_image), "canny");
+    
+    [L,Centers] = imsegkmeans(original_image,13);
+    image = label2rgb(L, im2double(Centers));
+    
+    image = rgb2hsv(image); 
+    % imtool(image);
+    h = image(:,:,1);
+    s = image(:,:,2);
+    v = image(:,:,3);
+    
+    red_mask = h > 0.92 & h < 1 & s > 0.5 & v > 0.2;
+    orange_mask = h > 0.04 & h < 0.12 & s > 0.7;
+    yellow_mask = h > 0.12 & h < 0.18;
+    white_mask = v > 0.7 & s < 0.4;
+    green_mask = h > 0.35 & h < 0.48;
+    blue_mask = h > 0.55 & h < 0.65 & s > 0.5;
+    black_mask = v < 0.4;
+    
+    se = strel("square", 25);
+    red_mask = imopen(red_mask, se);
+    orange_mask = imopen(orange_mask, se);
+    yellow_mask = imopen(yellow_mask, se);
+    white_mask = imopen(white_mask, se);
+    green_mask = imopen(green_mask, se);
+    blue_mask = imopen(blue_mask, se);
+    
+    subplot(3, 4, 1); imshow(original_image); title("original image");
+    subplot(3, 4, 2); imshow(hsv2rgb(image)); title("k-means");
+    subplot(3, 4, 3); imshow(image); title("hsv image");
+    subplot(3, 4, 4); imshow(red_mask); title("red mask");
+    subplot(3, 4, 5); imshow(orange_mask); title("orange mask");
+    subplot(3, 4, 6); imshow(yellow_mask); title("yellow mask");
+    subplot(3, 4, 7); imshow(white_mask); title("white mask");
+    subplot(3, 4, 8); imshow(green_mask); title("green mask");
+    subplot(3, 4, 9); imshow(blue_mask); title("blue mask");
+    subplot(3, 4, 10); imshow(black_mask); title("black mask");
+%     subplot(3, 4, 11); imshow(edges); title("edges");
+    
+    masks(:,:,1) = red_mask;
+    masks(:,:,2) = orange_mask;
+    masks(:,:,3) = yellow_mask;
+    masks(:,:,4) = white_mask;
+    masks(:,:,5) = green_mask;
+    masks(:,:,6) = blue_mask;
+    
+    grid = masksToGrid(masks, ["R", "O", "Y", "W", "G", "B", "-"]);
+    grid
+end
 
 function [grid] = masksToGrid(masks, colors) 
-    centroids = zeros(9, 3);
+    centroids = ones(9, 3)*7;
     counter = 1;
     for i=1:6
         cc = bwlabel(masks(:,:,i));
@@ -76,7 +80,6 @@ function [grid] = masksToGrid(masks, colors)
         end
     end
     centroids = sortrows(centroids, 1:2, ["ascend", "ascend"]);
-    grid = reshape(centroids(:,3), 3, 3);
-    disp(grid);
+    grid = reshape(centroids(1:9,3), 3, 3);
     grid = colors(grid)';
 end
